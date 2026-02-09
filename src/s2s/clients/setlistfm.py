1e98963ce5f01d0f948703f51863db1aee743cef
LNV <pechafer.clase@gmail.com>
2026-02-09 22:47:28 +0100

    Refactor SetlistFM client into a class and main application logic for improved structure and functionality.

--- src/s2s/clients/setlistfm.py
+++ src/s2s/clients/setlistfm.py
@@ -1,27 +1,82 @@
-from s2s.config.settings import settings
+from typing import Any, List
 import httpx
-import json
 
-base_url = "https://api.setlist.fm/rest/1.0/"
-
-
-def get_artist_mbid(artist_name: str, page: int = 1):
-    headers = {"Accept": "application/json", "x-api-key": settings.SETLISTFM_API_KEY}
-    endpoint = "search/artists"
-    parameters = {"artistName": artist_name, "p": page, "sort": "sortName"}
-
-    response = httpx.get(base_url + endpoint, params=parameters, headers=headers)
-
-    return json.dumps(response.json(), indent=4)
+BASE_URL = "https://api.setlist.fm/rest/1.0/"
 
 
-def test():
-    artist_name = input("Artist name: ")
+class SetlistFMClient:
 
-    response = get_artist_mbid(artist_name)
+    def __init__(self, api_key: str):
+        self.headers = {
+            "Accept": "application/json",
+            "x-api-key": api_key,
+        }
 
-    print(response)
+    def _get_artist(self, artist_name: str, page: int = 1):
+        url = f"{BASE_URL}search/artists"
+        parameters = {
+            "artistName": artist_name,
+            "p": page,
+            "sort": "sortName",
+            "timeout": 10,
+        }
 
+        response = httpx.get(url, params=parameters, headers=self.headers)
+        response.raise_for_status()
 
-if __name__ == "__main__":
-    test()
+        return response.json()
+
+    def _get_mbid(self, artist_name: str):
+        data = self._get_artist(artist_name)
+        artist = data.get("artist", None)[0]
+
+        if not artist:
+            return None
+
+        return artist.get("mbid")
+
+    def _get_setlist_data(self, artist_name: str, page: int = 1):
+        url = f"{BASE_URL}search/setlists"
+        artist_mbid = self._get_mbid(artist_name)
+
+        parameters = {
+            "artistMbid": artist_mbid,
+            "p": page,
+            "timeout": 10,
+        }
+
+        response = httpx.get(url, params=parameters, headers=self.headers)
+        response.raise_for_status()
+
+        return response.json()
+
+    def _get_setlist(self, artist_name: str):
+        setlist = []
+        data = self._get_setlist_data(artist_name)
+        setlist_data = data.get("setlist", [])[0]
+
+        if not setlist_data:
+            return None
+
+        sets = setlist_data.get("sets", [])
+
+        if not sets:
+            return None
+
+        set = sets.get("set", [])
+
+        for group in set:
+            songs = group.get("song", [])
+
+            if not songs:
+                return None
+
+            for song in songs:
+                song_name = song.get("name", "")
+
+                if not song_name:
+                    return None
+
+                setlist.append(song_name)
+
+        return setlist
--- src/s2s/main.py
+++ src/s2s/main.py
@@ -1,9 +1,20 @@
-from s2s.clients.setlistfm import test
+from s2s.config.settings import settings
+from s2s.clients.setlistfm import SetlistFMClient
+
+import json
 
 
 def main():
-    print("Hello from s2s!")
-    test()
+
+    client = SetlistFMClient(settings.SETLISTFM_API_KEY)
+
+    artist_name = "parkway drive"  # input("artista: ").strip()
+    setlist = client._get_setlist(artist_name)
+
+    if setlist:
+        print(f"Artist setlist: {setlist}")
+    else:
+        print(f"Artist not found")
 
 
 if __name__ == "__main__":
