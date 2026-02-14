4891d455d1722da3ca61bd68c0547045c04854a3
{Pedro Chao} <{pechafer.clase@gmail.com}>
2026-02-14 22:08:05 +0000

    Main adapted for the new setlistclient.

--- src/s2s/main.py
+++ src/s2s/main.py
@@ -1,15 +1,14 @@
 from s2s.config.settings import settings
 from s2s.clients.setlistfm import SetlistFMClient
 
-import json
-
-
 def main():
 
     client = SetlistFMClient(settings.SETLISTFM_API_KEY)
 
     artist_name = "parkway drive"  # input("artista: ").strip()
-    setlist = client._get_setlist(artist_name)
+
+
+    setlist = client.get_setlist(artist_name)
 
     if setlist:
         print(f"Artist setlist: {setlist}")
