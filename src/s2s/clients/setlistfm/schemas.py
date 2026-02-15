e9a255721f39d9d95f5e4c2bba1c4ab8b0229c70
{Pedro Chao} <{pechafer.clase@gmail.com}>
2026-02-15 21:35:10 +0000

    Added pydantic models for the setlistfm api client

--- src/s2s/clients/setlistfm/schemas.py
+++ src/s2s/clients/setlistfm/schemas.py
@@ -1,2 +1,18 @@
-from pydantic import BaseModel
+from typing import List
+from pydantic import BaseModel, Field
 
+
+class Song(BaseModel):
+    name: str
+
+class SetGroup(BaseModel):
+    song: List[Song] = Field(default_factory=list)
+
+class Sets(BaseModel):
+    set: List[SetGroup] = Field(default_factory=list)
+
+class Setlist(BaseModel):
+    sets: Sets
+
+class SetlistSearchResponse(BaseModel):
+    setlist: List[Setlist] = Field(default_factory=list)
