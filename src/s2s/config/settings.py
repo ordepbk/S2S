ca8bb7d280df03664d32b751ffe3c66b423ae3c4
LNV <pechafer.clase@gmail.com>
2026-02-20 19:30:54 +0100

    Added the spotify redirect uri for the code flow authorization.

--- src/s2s/config/settings.py
+++ src/s2s/config/settings.py
@@ -2,9 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SETLISTFM_API_KEY: str = ''
-    SPOTIFY_CLIENT_ID: str = ''
-    SPOTIFY_CLIENT_SECRET: str = ''
+    SETLISTFM_API_KEY: str = ""
+    SPOTIFY_CLIENT_ID: str = ""
+    SPOTIFY_CLIENT_SECRET: str = ""
+    SPOTIFY_REDIRECT_URI: str = ""
 
     model_config = SettingsConfigDict(
         env_file=".env",
