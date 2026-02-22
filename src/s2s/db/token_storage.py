5e309f56861eab72beb6cf1daca974bad2403d07
LNV <pechafer.clase@gmail.com>
2026-02-22 22:30:44 +0100

    Added function for temporal storage in the dataclass.

--- src/s2s/db/token_storage.py
+++ src/s2s/db/token_storage.py
@@ -1,9 +1,17 @@
 from dataclasses import field, dataclass
 from datetime import datetime
 
+from s2s.clients.spotify.schemas import ExchangeCodeResponse
+
 
 @dataclass
-class UserCredentials:
+class TokenData:
     refresh_token: str
     expires_in: int
     createddate: datetime = field(default_factory=datetime.now)
+
+
+def store_refresh_token(response: ExchangeCodeResponse):
+    user = TokenData(
+        refresh_token=response.refresh_token, expires_in=response.expires_in
+    )
