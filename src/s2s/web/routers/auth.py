98abd87b90d357ff43b8f8795533290d49cb4257
LNV <pechafer.clase@gmail.com>
2026-02-22 22:27:08 +0100

    Started callback router.

--- src/s2s/web/routers/auth.py
+++ src/s2s/web/routers/auth.py
@@ -1,11 +1,14 @@
 from fastapi.responses import RedirectResponse
 
-from s2s.web.app import app
-from s2s.services.auth_service import AuthService
+from s2s.web.app import app, auth_service
 
 
 @app.get("/login")
 async def login():
-    auth_handler = AuthService()
-    url = auth_handler.get_login_url()
+    url = auth_service.get_login_url()
     return RedirectResponse(url)
+
+
+@app.get("/callback")
+async def callback(code: str, state: str):
+    auth_service.callback_handler(code, state)
