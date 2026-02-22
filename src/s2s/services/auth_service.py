ff099c7572f1742df7d6334ba3f3664ff212c332
LNV <pechafer.clase@gmail.com>
2026-02-22 22:33:12 +0100

    Auth service development.

--- src/s2s/services/auth_service.py
+++ src/s2s/services/auth_service.py
@@ -1,47 +1,25 @@
-from datetime import datetime, timedelta
 import secrets
 
 from s2s.clients.spotify.auth import SpotifyAuth
-from s2s.db.token_storage import UserCredentials
-from s2s.clients.spotify.schemas import SpotifyAuthResponse
+from s2s.db.token_storage import store_refresh_token
+from s2s.clients.spotify.schemas import ExchangeCodeResponse
 
 
-def _generate_random_state() -> str:
-    return "xyz"
-
-
-def _store_refresh_token(response: SpotifyAuthResponse):
-    user = UserCredentials(
-        refresh_token=response.refresh_token, expires_in=response.expires_in
-    )
-
-    return user
-
-
-def get_updated_token(auth_client: SpotifyAuth, user: UserCredentials):
-    token_last_second = timedelta(seconds=user.expires_in, user.createddate)
-
-    if token_last_second > 0:
-        token = auth_client.update_token()
-
-    return token_last_second
-
-
-def main():
-    auth_client = SpotifyAuth()
-
-    state = _generate_random_state()
-    code = auth_client._get_auth_code(state)
-
-    response: SpotifyAuthResponse = auth_client.handle_callback(code, state, ...)
-
-    user = _store_refresh_token(response)
-
-
-class AuthService():
+class AuthService:
     def __init__(self):
         self.auth_manager = SpotifyAuth()
+        self.state = secrets.token_urlsafe(16)
 
     def get_login_url(self) -> str:
-        state = secrets.token_urlsafe(16)
-        return self.auth_manager.get_auth_code(state)
+        return self.auth_manager.get_auth_code(self.state)
+
+    async def callback_handler(self, code, state) -> ...:
+
+        if not state or self.state != state:
+            return None
+
+        token_data = await self.auth_manager.exchange_code_for_token(code)
+
+        store_refresh_token(token_data)
+
+        return token_data.access_token
